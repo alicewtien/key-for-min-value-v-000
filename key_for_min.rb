36f73dac419044eb450e8693559405dc9c19fa99
alice T <alicewtien@gmail.com>
2018-09-11 15:42:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   array = name_hash.to_a
   min_value = []
   array.each do |set|
-    set[1] <=> set[2] <=> set[3]
+    set[1] <=> set[2]
+    set[1] <=> set[3]
     min_value << set[1]
   end
   min_value[1]
