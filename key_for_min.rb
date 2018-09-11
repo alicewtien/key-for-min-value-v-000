1f6cab6b78e806845d528da6d0600f61083a1153
alice T <alicewtien@gmail.com>
2018-09-11 17:17:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,7 @@
 
 def key_for_min_value(name_hash)
   array = name_hash.to_a
-  if array[1][1] < array[2][1]
-    array[2].pop
-  elsif array[0][1] < array[1][1]
-    array[1].pop
-  else
-    array[0].shift
-  end
-  array[0][0]
+
+
+  
 end
