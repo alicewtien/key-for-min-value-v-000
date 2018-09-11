08b2375052dfcfae2320216557eb0acd8bcb5457
alice T <alicewtien@gmail.com>
2018-09-11 17:11:47 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   array = name_hash.to_a
   smallest = []
   if array[0][1] < array[1][1]
-    smallest << array[0]
+    smallest << array
   end
 
   smallest[0][0]
