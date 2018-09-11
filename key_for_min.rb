d08be1d08448f272c670ced43c1c00d9078ae268
alice T <alicewtien@gmail.com>
2018-09-11 18:45:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  array = name_hash.to_a
-  min_key = []
-  if array[1][1] < array[0][1] && array[1][1] < array[2][1]
-    min_key = array.delete_at(1)
-  elsif array[0][1] < array[1][1] && array[0][1] < array[2][1]
-    min_key = array.shift
-  else
-    min_key = array.pop
-  end
-  min_key[0]
+  
 end
