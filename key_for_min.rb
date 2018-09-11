df8368797272c85c3b4179330dcfba01d079883b
alice T <alicewtien@gmail.com>
2018-09-11 16:58:16 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
   array = name_hash.to_a
   smallest = []
   array.each do |set|
-    if set[0[1]] > set[1[1]]
+    if set[0][1] > set[1][1]
       set[0].shift
     end
   end
-  array[0[0]]
+  array[0][0]
 end
