03acb8f53d74f073d0b7bbafbc83e815f9fc3f48
alice T <alicewtien@gmail.com>
2018-09-11 16:51:56 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  array = name_hash.to_a
-  array.map do |set|
-    set[1[1]] <=> set[2[1]]
-    set[0[1]] <=> set[1[1]]
-    end
-  array[0[0]]
+  name_hash.sort_by { |key, value| value.length }
 end
