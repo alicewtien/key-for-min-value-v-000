1835937c06d26a6da4a0dfe230ad5606bc32e48d
alice T <alicewtien@gmail.com>
2018-09-11 18:25:59 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,14 @@
 def key_for_min_value(name_hash)
   array = name_hash.to_a
   min_key = []
-  if array[1][1] < array[0][1] && array[1][1] < array[2][1]
+  if array - []
+    nil
+  elsif array[1][1] < array[0][1] && array[1][1] < array[2][1]
     min_key = array.delete_at(1)
   elsif array[0][1] < array[1][1] && array[0][1] < array[2][1]
     min_key = array.shift
-  elsif array[2][1] < array[1][1] && array[2][1] < array[0][1]
+  else
     min_key = array.pop
-  else array = []
-    nil
   end
   min_key[0]
 end
